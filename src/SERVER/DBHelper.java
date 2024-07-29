2e7a041f5099d8eaa76919519c4e14a70ab0ac0f
Asus <Asus@172.16.69.9>
2024-07-29 15:38:14 +0700

    29/07/2024:Ban dau tien cua phan mem (Gia Long - Trọng Phúc)

---
+++ src/SERVER/DBHelper.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SERVER;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Tung
+ */
+public class DBHelper {
+   Connection con;
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
+    public DBHelper() {
+       try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=StayCoffee2;user=sa;password=2khong1hai2khong0nam;encrypt=false ";
+            con = DriverManager.getConnection(url);
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Lỗi 100:: Không tìm thấy lớp");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Lỗi 101:: Không thể kết nối đến máy chủ");
+        } catch (NullPointerException e) {
+            JOptionPane.showMessageDialog(null, "Lỗi 102:: Cấu hình bị trống");
+        } catch (Exception e) {
+        }
+    }
+
+}
